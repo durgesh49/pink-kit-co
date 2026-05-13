da15f0405d014f8e305f31ea96bdd37450e39187
durgesh49 <shivayofficialyt@gmail.com>
2026-05-12 21:26:32 -0800

    fixed provider

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import "./index.css";
 
 import { ShopProvider } from "./context/ShopContext";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <BrowserRouter basename="/pink-kit-co">
       <ShopProvider>
